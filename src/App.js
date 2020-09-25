5299cd93a82fbdd7359eeb2496a35d594a60fa92
Anik Ghosh <tech.anikghosh@gmail.com>
2020-09-25 16:25:24 +0530

    main structure

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./project.theme";
 
 // components
-import Navbar from "./client/components/NavBar/Navbar";
 import SignUp from "./client/components/SignUp/SignUp";
 import SignIn from "./client/components/SignIn/SignIn";
 import SignOut from "./client/components/SignOut/SignOut";
 import Profile from "./client/components/Profile/Profile";
 import Notification from "./client/components/Notification/Notification";
 import ForgetPassword from "./client/components/ForgetPassword/ForgetPassword";
+import SidePanel from "./client/components/SidePanel/SidePanel";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth="xl">
-        <Navbar />
         <Notification />
         <SignUp />
         <SignIn />
         <SignOut />
         <Profile />
         <ForgetPassword />
+        <SidePanel />
       </Container>
     </ThemeProvider>
   );
